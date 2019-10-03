1129124bdf2860c6d94e8969def6019983c82900
Kaho Cheung <kaho@elefanttraks.com>
2019-10-03 17:08:18 +1000

    Add Apollo and books query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,80 @@
 import React from 'react';
 import Head from 'next/head';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+import { withApollo } from '../lib/apollo';
+// import { Query } from 'apollo-client';
+
 import Nav from '../components/nav';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/static/favicon.ico" importance="low" />
-    </Head>
+const BOOKS = gql`
+  {
+    offTheShelf {
+      books {
+        id
+        title
+      }
+    }
+  }
+`;
 
-    <Nav />
+const Home = () => {
+  const {
+    // loading,
+    // error,
+    // data = {
+    //   offTheShelf: {
+    //     books: [],
+    //   },
+    // },
+    data,
+  } = useQuery(BOOKS, {
+    ssr: true,
+  });
 
-    <div className="hero">
-      <h1 className="title">Coming soon!</h1>
-      <p>testing 1</p>
+  // const { books } = data.offTheShelf;
+
+  const books = data && data.offTheShelf && data.offTheShelf.books;
+
+  console.log(books);
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/static/favicon.ico" importance="low" />
+      </Head>
+
+      <Nav />
+
+      <div className="hero">
+        <h1 className="title">Off the Shelf</h1>
+        {books &&
+          books.map((book) => {
+            return <p key={book.id}>{book.title}</p>;
+          })}
+      </div>
+
+      <style jsx>{`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          padding-top: 80px;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+      `}</style>
     </div>
+  );
+};
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
-
-export default Home;
+export default withApollo(Home);
