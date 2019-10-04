b3ca55cc1302da6933f102372436b45fb16c07e7
Kaho Cheung <kaho@elefanttraks.com>
2019-10-04 17:37:41 +1000

    Fix book modal test

--- components/BookCardModal/BookCardModal.test.tsx
+++ components/BookCardModal/BookCardModal.test.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import BookCardModal from './BookCardModal';
+
+import { initApolloClient } from '../../lib/apollo';
+
+const client = initApolloClient({});
 
 afterEach(cleanup);
 
 describe('BookCardModal', () => {
   it('renders BookCardModal component', () => {
-    render(<BookCardModal />);
+    render(
+      <ApolloProvider client={client}>
+        <BookCardModal />
+      </ApolloProvider>,
+    );
   });
 });
