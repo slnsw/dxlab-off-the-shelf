aa4207506a4ec7644f385ac6054597a7284461c6
Luke Dearnley <luke.dearnley@sl.nsw.gov.au>
2019-10-16 19:31:10 +1100

    random heights for spines

--- components/BookSpine/BookSpine.tsx
+++ components/BookSpine/BookSpine.tsx
@@ -11,11 +11,20 @@ const BookSpine: React.FunctionComponent<Props> = ({ className, id }) => {
   if (id < 1 || id > 96) {
     return null;
   }
+  const h = id % 5; // Math.floor(Math.random() * 5);
+
   return (
     <div className={[css.bookSpine, className || ''].join(' ')}>
       <img
         src={`/images/spines/${id}.png`}
-        className={css.image}
+        className={[
+          css.image,
+          h === 0 ? css.height0 : '',
+          h === 1 ? css.height1 : '',
+          h === 2 ? css.height2 : '',
+          h === 3 ? css.height3 : '',
+          h === 4 ? css.height4 : '',
+        ].join(' ')}
         // lazy="true"
         alt="spine"
       />
