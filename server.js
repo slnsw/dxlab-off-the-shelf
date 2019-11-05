dcf59b4a21909bd9e1f926a8f03a81bb82a6ffea
Kaho Cheung <kaho@elefanttraks.com>
2019-11-05 16:34:42 +1100

    Update changelog

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+/**
+ * WARNING: Only use this for local development
+ *
+ * This server.js file enables assetPrefix in next.config.js to work in local
+ * development. Server.js doesn't seem to work when deployed by Now, so
+ * now.json and now.staging.json has a 'routes' rule that proxies
+ * '/off-the-shelf/assets/_next/' to '/_next/'
+ */
+
 app.prepare().then(() => {
   const server = express();
 
-  // Enable assetPrefix in next.config.js to work
   server.use('/off-the-shelf/assets', app.getRequestHandler());
 
   server.all('*', (req, res) => {
