cebb3a2fa97f1e077fd512f45c5d6e352e925700
Kaho Cheung <kaho@elefanttraks.com>
2020-02-07 12:49:34 +1100

    Add page security function

---
+++ lib/check-page-security.ts
@@ -0,0 +1,42 @@
+import { NextPageContext } from 'next';
+
+/**
+ * Analyse Next JS's `getInitialProps` headers for security purposes
+ * @param nextPageContext
+ */
+const checkPageSecurity = ({ req }: NextPageContext): boolean => {
+  // --------------------------------------------------------------------------
+  // EDIT: Add whitelisted ips or hosts below:
+  const allowedIps = [process.env.OFF_THE_SHELF_CLIENT_IP];
+  const allowedHosts = ['dxlab.sl.nsw.gov.au'];
+  // --------------------------------------------------------------------------
+
+  // Do not allow access by default
+  let allowAccess = false;
+
+  const isProduction = process.env.NODE_ENV === 'production';
+  // const isProduction = true;
+
+  const ip = (
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    ''
+  )
+    .split(',')[0]
+    .trim();
+  const { host } = req.headers;
+
+  if (isProduction) {
+    // Allow access if host or ip is whitelisted
+    if (allowedHosts.includes(host) || allowedIps.includes(ip)) {
+      allowAccess = true;
+    }
+  } else {
+    // Allow access for development, ie. not on production.
+    allowAccess = true;
+  }
+
+  return allowAccess;
+};
+
+export default checkPageSecurity;
--- pages/loader.tsx
+++ pages/loader.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Router from 'next/router';
 
 import Loader from '../components/Loader';
+import checkPageSecurity from '../lib/check-page-security';
 
 const LoaderPage = () => {
   const [isActive, setIsActive] = React.useState(true);
@@ -20,6 +22,27 @@ const LoaderPage = () => {
       <Loader isActive={isActive} strokeWidth={4} delay={0} />
     </div>
   );
+};
+
+const redirectUrl = 'https://dxlab.sl.nsw.gov.au/off-the-shelf';
+
+LoaderPage.getInitialProps = (context) => {
+  const { res } = context;
+
+  if (checkPageSecurity(context)) {
+    return {};
+  }
+
+  if (res) {
+    res.writeHead(302, {
+      Location: redirectUrl,
+    });
+    res.end();
+  } else {
+    Router.push(redirectUrl);
+  }
+
+  return {};
 };
 
 export default LoaderPage;
